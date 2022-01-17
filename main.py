59b5f1f0521e7521566d06a367bdb977414af3b8
SilviH <hrbkova.s@email.cz>
2022-01-17 17:21:30 +0100

    Train and test set
    Splitting data into train and test set.
    
    Setting the independent and dependent variables within the sets.

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
 import pandas as pd
+from sklearn.model_selection import train_test_split
 
 review = pd.read_csv('IMDB Dataset.csv')
 
 # Creation of smaller and balanced dataset to train the model faster in first steps
 review_positive = review[review['sentiment'] == 'positive'][:1000]
 review_negative = review[review['sentiment'] == 'negative'][:1000]
-review_imb = pd.concat([review_positive, review_negative])
+review_bal = pd.concat([review_positive, review_negative])
+
+# Splitting data into train and test set
+train, test = train_test_split(review_bal, test_size=0.33, random_state=17)
+
+# Data overview
+print('Number of positive reviews in train:', len(train[train['sentiment'] == 'positive']))
+print('Number of negative reviews in train:', len(train[train['sentiment'] == 'negative']))
+print('Number of positive reviews in test:', len(test[test['sentiment'] == 'positive']))
+print('Number of negative reviews in test:', len(test[test['sentiment'] == 'negative']))
+
+# Set the independent variables (review) and dependent variables (sentiment) within the train and test set
+train_x, train_y = train['review'], train['sentiment']
+test_x, test_y = test['review'], test['sentiment']
